f39f14bc3dcd22da32b74853f26696d439174cea
Alexander Mikhailov <45492097+MikhailovAlexander@users.noreply.github.com>
2025-03-26 18:29:57 +0000

    add poetry

---
+++ src/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """Класс конфигурируемые параметры приложения. Параметры могут быть переопределены
+    в файле .env в корне проекта или через переменные окружения."""
+
+    LOG_LEVEL: str = "WARNING"
+    TELEGRAM_TOKEN: str = "token-value"
+    RESEND_CHAT_ID: int = 0
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=True
+    )
