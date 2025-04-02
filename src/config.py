155be6f8d50d183e7982d8a4c6f5d42aa823ac9b
Sven_Packmaster <koliaagapov17@gmail.com>
2025-04-02 19:14:13 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,8 +6,8 @@ class Settings(BaseSettings):
     в файле .env в корне проекта или через переменные окружения."""
 
     LOG_LEVEL: str = "WARNING"
-    TELEGRAM_TOKEN: str = "7761012427:AAG2LJi0yNKxj3AEs0WLmwpQRU0SzVsxcM0"
-    RESEND_CHAT_ID: int = "1259813535"
+    TELEGRAM_TOKEN: str = "your_token"
+    RESEND_CHAT_ID: int =  0
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=True
